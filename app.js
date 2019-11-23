85dea51b32cf5657f26fda17cffa29fd28d6bc90
gassoth <ohthatemailaddress@gmail.com>
2019-11-22 19:48:58 -0500

    simple login

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
+//CURRENT STATUS
+//passport is authenticating and redirecting, i want to confirm if its actually working though.
+
+//requires
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/logins');
+const passport = require('passport');
+var session = require('express-session');
+const flash = require('connect-flash');
+const { Model } = require('objection');
+
+const Knex = require('knex');
+const knexConfig = require('./knexfile');
+const knex = Knex(knexConfig.development);
+// Bind all Models to a knex instance. If you only have one database in
+// your server this is all you have to do. For multi database systems, see
+// the Model.bindKnex method.
+Model.knex(knex);
 
 var app = express();
 
@@ -14,6 +30,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,8 +43,28 @@ app.use(sassMiddleware({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+//passport config
+require('./passport.js')(passport);
+app.use(session({
+    secret: 'abcdefg',
+    resave: true,
+    saveUninitialized: false
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+//routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+
+//logout route
+app.get('/logout',
+	function(req, res) {
+	    req.logout();
+	    res.redirect('/');
+	});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,5 +81,10 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.use(function(err, req, res, next) {
+    console.log(err);
+});
+
 
 module.exports = app;
