d4bf129c766e8879ca507bc68badff43b6f7a8a0
gassoth <ohthatemailaddress@gmail.com>
2020-02-05 22:46:24 -0500

    write page writes quill delta to node console not yet pushed to db

--- controllers/writeController.js
+++ controllers/writeController.js
@@ -1,5 +1,29 @@
+const { body,validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
 
 //Controller to get static page
 exports.get_write = (req, res) => {
-    res.render('write');
+    if (req.user) {
+        res.render('write');
+    } else {
+        res.redirect('/login');
+    }
 }
+
+exports.post_write = [
+
+    //sanitize
+    sanitizeBody('postText'),
+    sanitizeBody('title'),
+
+    //test
+    (req, res, next) => {
+        //var delta = req.body.editor.getContents();
+        //console.log(req.body.title);
+        console.log(req.body.title);
+        console.log(req.body.postText);
+        res.redirect('/');
+    }
+
+
+]
--- routes/write.js
+++ routes/write.js
@@ -2,10 +2,13 @@
 var express = require('express');
 var router = express.Router();
 
-//Require login controller modules
+//Require write controller modules
 var write_controller = require('../controllers/writeController.js');
 
-//Get feed route
+//Get write route
 router.get('/', write_controller.get_write);
+
+//post write (create blogpost)
+router.post('/', write_controller.post_write);
 
 module.exports = router;
